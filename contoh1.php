c3b6d5e453fc3930998da685e0815d781b6e3183
Achmad Baidillah <54340341+livat3564@users.noreply.github.com>
2023-09-30 12:47:23 +0700

    Add files via upload

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Achmad Baidillah
+              saya tinggal di daerah krukut
+              olah raga yang saya sukai adalah
+              sepak bola";   
+    }
+}
